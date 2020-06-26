b6d88745444d0d514d36d122fa63b071f1a977f5
Aaron Rodden <aaron.rodden21@gmail.com>
2020-06-26 14:30:06 -0700

    Added a grid, added an x sprite that can be dragged. A lot of design is going to need to be done

--- js/ticTacToe.js
+++ js/ticTacToe.js
@@ -1,9 +1,12 @@
+/*
+*
+* PIXI Setup
+*
+*/
 let type = "WebGL"
 if(!PIXI.utils.isWebGLSupported()){
   type = "canvas"
 }
-
-PIXI.utils.sayHello(type)
 
 //Aliases
 let Application = PIXI.Application,
@@ -31,69 +34,171 @@ app.renderer.view.style.display = "block";
 app.renderer.autoDensity = true;
 app.renderer.resize(window.innerWidth, window.innerHeight);
 
-//TODO: Make a grid of squares
+
 //TODO: Make each square clickable and know which one is clicked
-
-//create game screen and contents
-let squareHeight = 150;
-let squareWidth = 150;
-
-gameBoard = new PIXI.Container();
-
-let redRect = new PIXI.Graphics();
-redRect.beginFill(0xFF0000);
-redRect.drawRect(0,0, squareHeight, squareWidth);
-redRect.interactive = true;
-redRect.on('mousedown', onDown);
-gameBoard.addChild(redRect);
-
-let otherRect = new PIXI.Graphics();
-otherRect.beginFill(0xDD4444);
-otherRect.drawRect(200,200, squareHeight, squareWidth);
-otherRect.interactive = true;
-otherRect.on('click', onClick);
-gameBoard.addChild(otherRect);
-app.stage.addChild(gameBoard);
 
 function onClick (eventData) {
     console.log(eventData.data);
     console.log("Clicked a square");
 }
 
-// function createGrid() {
-//     var grid = {};
-//     // const container = document.getElementById("container");
-//     // const container = document.createElement("div");
-//     // container.setAttribute('id', 'container')
-//     // document.getElementsByTagName("canvas")[0].appendChild(container);
-//     for (var row = 0; row < 3; row++){
-//         for (var col = 0; col < 3; col++) {
-//             if ((row+col) % 2 == 0){
-//                 let square = new Sprite(resources["images/brown-square.png"].texture);
-//                 square.height = squareHeight;
-//                 square.width = squareWidth;
-//                 square.x = (row+col) * 25;
-//                 square.y = (row+col) * 25;
-//                 $("#container").append("<div class='grid'></div>");
-//                 grid[row+col] = square;
-//             }
-//             else {
-//                 let square = new Sprite(resources["images/lightbrown-square.png"].texture);
-//                 square.height = squareHeight;
-//                 square.width = squareWidth;
-//                 square.x = (row+col) * 25;
-//                 square.y = (row+col) * 25;
-//                 // $("#container").append("<div class='grid'></div>");
-//                 grid[row+col] = square;
-//             }
-//         }
-//     }
-//     // $(".grid").width(960/3);
-//     // $(".grid").height(960/3);
-//
-//     return grid;
-// }
-//
+function createGrid(gameBoard) {
+    var columns = 3;
+    var rows = 3;
+
+    var grid = new Array(columns);
+    for (var i = 0; i < grid.length; i++) {
+        grid[i] = new Array(rows);
+    }
+
+    let cellSize = {
+        x : 150,
+        y : 150
+    }
+
+    for (var i = 0; i < columns; i++) {
+        for (var j = 0; j < rows; j++) {
+            let cell = new PIXI.Graphics();
+            if ((i+j) % 2 == 0) {
+                cell.beginFill(0x8B4513);
+            }
+            else {
+                cell.beginFill(0xFFF8DC);
+            }
+            cell.interactive = true;
+            cell.on('click', onClick);
+            cell.drawRect(i*cellSize.x + 5.0, j*cellSize.y + 5.0, cellSize.x, cellSize.y);
+            grid[i][j] = cell;
+            gameBoard.addChild(cell);
+        }
+    }
+    return grid;
+}
+
+function onDragStart(event)
+{
+    // store a reference to the data
+    // the reason for this is because of multitouch
+    // we want to track the movement of this particular touch
+    this.data = event.data;
+    this.alpha = 0.5;
+    this.dragging = true;
+}
+
+function onDragEnd()
+{
+    // We have postion of object at end of drag
+    // console.log(this.position.x);
+    // console.log(this.position.y);
+
+    this.alpha = 1;
+
+    this.dragging = false;
+
+    // set the interaction data to null
+    this.data = null;
+}
+
+function onDragMove()
+{
+    if (this.dragging)
+    {
+        var newPosition = this.data.getLocalPosition(this.parent);
+        this.position.x = newPosition.x;
+        this.position.y = newPosition.y;
+    }
+}
+
+var xTexture = PIXI.Texture.fromImage('images/x.png');
+var circleTexture = PIXI.Texture.fromImage('images/circle.png');
+
+function createPiece(stage, playerID) {
+    var shape;
+    if (playerID == 0) {
+        shape = new PIXI.Sprite(circleTexture);
+    }
+    else {
+        shape = new PIXI.Sprite(xTexture);
+    }
+
+    shape.widhth = 100;
+    shape.height = 100;
+    // shape.scale.set(0.00125,0.00125);
+    // enable the shape to be interactive... this will allow it to respond to mouse and touch events
+   shape.interactive = true;
+   // this button mode will mean the hand cursor appears when you roll over the shape with your mouse
+   shape.buttonMode = true;
+   // center the shape's anchor point
+   shape.anchor.set(0.5);
+   // make it a bit bigger, so it's easier to grab
+   shape.scale.set(3);
+
+    // setup events
+    shape
+        // events for drag start
+        .on('mousedown', onDragStart)
+        .on('touchstart', onDragStart)
+        // events for drag end
+        .on('mouseup', onDragEnd)
+        .on('mouseupoutside', onDragEnd)
+        .on('touchend', onDragEnd)
+        .on('touchendoutside', onDragEnd)
+        // events for drag move
+        .on('mousemove', onDragMove)
+        .on('touchmove', onDragMove);
+
+    // move the sprite to its designated position
+    shape.position.x = 300;
+    shape.position.y = 300;
+
+    // add it to the stage
+    stage.addChild(shape);
+}
+
+// PIXI tutorial reccomended this
+let state;
+state = play
+
+var activePiece = false
+
+function gameLoop() {
+    state();
+    //Call this `gameLoop` function on the next screen refresh
+    //(which happens 60 times per second)
+    requestAnimationFrame(gameLoop);
+}
+
+function play(){
+    if (activePiece == false) {
+        createPiece(app.stage, 1);
+        activePiece = true
+    }
+}
+
+
+$(document).ready(function() {
+    /*
+    *
+    * Main game Setup
+    *
+    */
+
+    // Setup game screen
+    gameBoard = new PIXI.Container();
+    graphicsGrid = createGrid(gameBoard);
+    //Add game board to stage
+    app.stage.addChild(gameBoard);
+
+    //Start the loop
+    gameLoop();
+});
+
+
+/*
+*
+* Sprite loading example
+*/
+
 // PIXI.loader
 //   .add("images/brown-square.png")
 //   .add("images/lightbrown-square.png")
